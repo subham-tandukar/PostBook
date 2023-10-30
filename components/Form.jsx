b1dd597e90a00d1cf7bbb8235bdba65d0d8bcc53
Subham <subhamtndkr9@gmail.com>
2023-10-30 22:40:06 +0545

    commit

--- components/Form.jsx
+++ components/Form.jsx
@@ -13,7 +13,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
 
       <form
         onSubmit={handleSubmit}
-        className="mt-10 pb-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+        className="section w-full max-w-2xl flex flex-col gap-7 glassmorphism"
       >
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
